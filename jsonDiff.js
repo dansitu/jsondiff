921a9762fbc3d3f83e46e5271d0a5d5b940a1cf9
Daniel Imrie-Situnayake <dansmailbag@googlemail.com>
2017-03-12 18:11:35 -0700

    Turning into a library

--- jsonDiff.js
+++ jsonDiff.js
@@ -46,52 +46,61 @@ JSONDiff.deepDiff = function (original, replacement) {
 
   var edits = [];
 
+  // Record any updates or removals
   Object.keys(original).forEach(function(key) {
-    var originalProperty = original[key];
-
-    // If item no longer present, remove it
-    if(!replacement.hasOwnProperty(key)) {
-      edits.push(JSONDiff.removeKey(key));
-      return;
-    }
-
-    var newProperty = replacement[key];
-
-    // If item is a primitive, compare equality
-    if(JSONDiff.isPrimitive(originalProperty)) {
-      if(original[key] !== replacement[key]) {
-        edits.push(JSONDiff.replaceKey(key, newProperty));
-      }
-      return;
-    }
-
-    // If an array, replace it
-    if(JSONDiff.isArray(originalProperty)) {
-      edits.push(JSONDiff.replaceKey(key, newProperty));
-      return;
-    }
-
-    // Otherwise, this property is an object.
-    // If the new one is also an object, recurse
-    if(JSONDiff.isObject(newProperty)) {
-      var subdiff = JSONDiff.deepDiff(originalProperty, newProperty, key);
-      if(subdiff.length) edits.push(JSONDiff.editKey(key, subdiff));
-    } else {
-      // Otherwise, just replace it
-      edits.push(JSONDiff.replaceKey(key, newProperty));
-    }
-
+    JSONDiff.recordUpdates(key, original, replacement, edits);
   });
 
-  // Check for any additions
+  // Record any additions
   Object.keys(replacement).forEach(function(key) {
-    if(!original.hasOwnProperty(key)) {
-      edits.push(JSONDiff.addKey(key, replacement[key]));
-    }
+    JSONDiff.recordAddition(key, original, replacement, edits);
   });
 
   return edits;
 
+};
+
+JSONDiff.recordUpdates = function(key, original, replacement, edits) {
+  var originalProperty = original[key];
+
+  // If item no longer present, remove it
+  if(!replacement.hasOwnProperty(key)) {
+    edits.push(JSONDiff.removeKey(key));
+    return;
+  }
+
+  var newProperty = replacement[key];
+
+  // If item is a primitive, compare equality
+  if(JSONDiff.isPrimitive(originalProperty)) {
+    if(original[key] !== replacement[key]) {
+      edits.push(JSONDiff.replaceKey(key, newProperty));
+    }
+    return;
+  }
+
+  // If an array, replace it
+  if(JSONDiff.isArray(originalProperty)) {
+    edits.push(JSONDiff.replaceKey(key, newProperty));
+    return;
+  }
+
+  // Otherwise, this property is an object.
+  // If the new one is also an object, recurse
+  if(JSONDiff.isObject(newProperty)) {
+    var subdiff = JSONDiff.deepDiff(originalProperty, newProperty, key);
+    if(subdiff.length) edits.push(JSONDiff.editKey(key, subdiff));
+  } else {
+    // Otherwise, just replace it
+    edits.push(JSONDiff.replaceKey(key, newProperty));
+  }
+
+};
+
+JSONDiff.recordAddition = function(key, original, replacement, edits) {
+  if(!original.hasOwnProperty(key)) {
+    edits.push(JSONDiff.addKey(key, replacement[key]));
+  }
 };
 
 JSONDiff.replaceKey = function(key, value) {
